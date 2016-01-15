56a335da6e2522192dc1c07a71d3b3ac14b513a5
Sallust <daniyom@gmail.com>
2016-01-15 13:21:30 -0500

    Initial Commit
    -basic file structure
    -knockout and bootstrap
    -gulp downloaded

---
+++ src/js/app.js
@@ -0,0 +1,78 @@
+var initialCats = [
+	{
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/tabby.jpg',
+		imgAttribution: 'http://www.flickr.com',
+		nicknames: ["The Cutest!!", "Almost as Cute", "As cute as Unicorn Smiles", "The Cat"]
+	},
+	{
+		clickCount: 0,
+		name: 'Sleepy',
+		imgSrc: 'img/sleepy.jpg',
+		imgAttribution: 'http://www.flickr.com',
+		nicknames: ["Super the sleepiest!!!", "Almost as Cute", "As cute as Unicorn Smiles", "The Cat"]
+	},
+	{
+		clickCount: 0,
+		name: 'Lioness',
+		imgSrc: 'img/rawr.jpg',
+		imgAttribution: 'http://www.flickr.com',
+		nicknames: ["King of Kute", "Cub with the Cute", "As cute as Unicorn Smiles", "The Cat"]
+	},
+	{
+		clickCount: 0,
+		name: 'Sarah Rebecca Elizabeth Windsor',
+		imgSrc: 'img/lawncat.jpg',
+		imgAttribution: 'http://www.flickr.com',
+		nicknames: ["Her Royal Meowjesty", "Princess of Purrdom", "As cute as Unicorn Smiles", "The Cat"]
+	},
+	{
+		clickCount: 0,
+		name: 'The Donald',
+		imgSrc: 'img/fatcat.jpg',
+		imgAttribution: 'http://www.flickr.com',
+		nicknames: ["Present!", "R U Still here?", "Hair for days", "The Cat"]
+	}
+]
+
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+
+	this.catLevelArray = ["infant","toddler", "pre-teen", "annoying teen", "teen", "adolescent", "adult", "dead...awkward. You totally killed it"]
+
+	this.catLevel = ko.computed(function() {
+		return this.catLevelArray[Math.floor(this.clickCount()/10)];
+	},this)
+
+	this.nicknames = ko.observable(data.nicknames);
+}
+
+var ViewModel = function() {
+	var self = this;
+	console.log("sup");
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem){
+		self.catList.push( new Cat(catItem) );
+	});
+
+	self.currentCat = ko.observable( self.catList()[1] );
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+	self.setCurrentCat = function() {
+		//console.log(self.currentCat())
+		self.currentCat ( this );
+	};
+}
+
+ko.applyBindings(new ViewModel())
+
+//[Math.floor(this.clickCount / 10)

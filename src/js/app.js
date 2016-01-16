3faeca28800437a8e150b8887a6eaf858f443d0a
Sallust <daniyom@gmail.com>
2016-01-15 22:04:19 -0500

    feat: textSearch working
    -solution by putting call to library script in same line as initial map  library

--- src/js/app.js
+++ src/js/app.js
@@ -36,6 +36,71 @@ var initialCats = [
 	}
 ]
 
+var topPicks = ["Jefferson Vineyards", "Monticello", "University of Virginia", "Downtown Mall", "Ash Lawn-Highland"];
+
+var MapFunc = {
+	map: null,
+	mapOptions: {
+		center: {lat: 38.031, lng: -78.486},
+    	zoom: 8,
+    	disableDefaultUI: true
+  	},
+	callback: function(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+      	console.log(results);
+    	}
+	},
+	init: function () {
+		this.map = new google.maps.Map(document.querySelector('#map'), this.mapOptions);
+		var service = new google.maps.places.PlacesService(this.map);
+
+		service.textSearch({query: "The White House"}, this.callback)
+
+
+	},
+	test: function() {
+		console.log("I'm bear")
+		function callback(results, status) {
+			console.log(status);
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      console.log("I'm alive");
+    }
+  }
+
+		var service = new google.maps.places.PlacesService(this.map);
+		service.nearbySearch({quhery: "The White House"}, callback)
+
+	}
+
+
+}
+
+
+var Place = function(placeName) {
+	console.log(placeName);
+	var self = this;
+	//var service;
+	var request = { query: placeName}
+
+	//service = new google.maps.places.PlacesService(map);
+	function callback(results, status) {
+		console.log(results)
+	}
+
+  	//service.textSearch(request, callback);
+  	//console.log(service)
+/*
+	var callback = function(results, status) {
+		console.log("I'm here")
+			if (status == google.maps.places.PlacesServiceStatus.OK) {
+				console.log(results)
+	  		}
+		}
+*/
+
+	//service.textSearch(request, callback);
+
+}
 
 var Cat = function(data) {
 	this.clickCount = ko.observable(data.clickCount);
@@ -53,10 +118,14 @@ var Cat = function(data) {
 }
 
 var ViewModel = function() {
+	MapFunc.init();
+
 	var self = this;
 	console.log("sup");
 
-	this.catList = ko.observableArray([]);
+	self.catList = ko.observableArray([]);
+
+
 
 	initialCats.forEach(function(catItem){
 		self.catList.push( new Cat(catItem) );
@@ -76,12 +145,39 @@ var ViewModel = function() {
     	zoom: 8,
     	disableDefaultUI: true
   	};
-	this.map = null;
-	self.initializeMap = function() {
-		this.map = new google.maps.Map(document.querySelector('#map'), this.mapOptions);
-	}
-}
+	//this.map = null;
+	//self.initializeMap = function() {
+	self.placeList = ko.observableArray([]);
 
-ko.applyBindings(new ViewModel())
+	//this.service = new google.maps.places.PlacesService(self.map);
+/*
+	topPicks.forEach(function(placeName){
+		self.placeList.push( new Place(placeName))
+	}); */
+	function pleaseWork() {
+		console.log("Mana from heaven")
+	}
+	var request = {
+        query: "The White House"
+      };
+      //console.log(self.service.textSearch)
+
+
+	topPicks.forEach(function(placeName) {
+		//self.textSearch({query:placeName}, function() {
+			console.log("Please god")
+		})
+
+		//console.log("brought to you by insanity")
+
+	//pleaseWork();
+
+	this.callback = function(results, status) {
+		console.log(status);
+	}
+	//}
+}
+//This now is called by google map success callback
+//ko.applyBindings(new ViewModel())
 
 //[Math.floor(this.clickCount / 10)
